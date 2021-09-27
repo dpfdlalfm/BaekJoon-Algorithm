8109274d2d164520e10f2bbf96eef9207b3c250a
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2021-09-27 14:58:54 +0900

    Update danji beonho.py

--- category/bfs dfs/danji beonho.py
+++ category/bfs dfs/danji beonho.py
@@ -2,45 +2,51 @@ from collections import deque
 import sys
 
 n = int(input())
-if 5 <= n <= 25:pass
-else: sys.exit()
+if 5 <= n <= 25:
+    pass
+else:
+    sys.exit()
 
 aparts = []
 for _ in range(n):
     aparts.append(list(input()))
 
+go_i = [-1, 1, 0, 0]
+go_j = [0, 0, -1, 1]
+
+
 visited = [[False] * n for _ in range(n)]
-print(visited)
+house_num = []
 
 
-def move(i,j):
-    cnt = 0
-    while cnt < 4:
-        if i < 0 or i > n or j < 0 or j > n:
-            continue
-
-
-def bfs(i,j):
+def bfs(i, j):
     queue = deque()
-    queue.append([i,j])
+    queue.append([i, j])
     visited[i][j] = True
+    cnt = 1
     while queue:
         i, j = queue.popleft()
-        if i+1 <= n and aparts[i+1][j] == '1' and not visited[i+1][j]:
-            visited[i+1][j] = True
-            queue.append([i,j+1])
-        if i-1 >= 0 and aparts[i-1][j] == '1' and not visited[i-1][j]:
-            visited[i-1][j] = True
-            queue.append([i-1,j])
-        if j+1 <= n and aparts[i][j+1] == '1' and not visited[i][j+1]:
-            visited[i][j+1] = True
-            queue.append([i,j+1])
-        if i-1 >= 0 and aparts[i][j-1] == '1' and not visited[i][j-1]:
-            visited[i][j-1] = True
-            queue.append([i,j-1])
+        stopper = 0
+        while stopper < 4:
+            di = i + go_i[stopper]
+            dj = j + go_j[stopper]
+            stopper += 1
+            if di < 0 or di >= n or dj < 0 or dj >= n:
+                continue
+            if not visited[di][dj] and aparts[di][dj] == '1':
+                cnt += 1
+                visited[di][dj] = True
+                queue.append([di, dj])
+
+    house_num.append(cnt)
 
 
 for i in range(n):
     for j in range(n):
         if aparts[i][j] == '1' and not visited[i][j]:
-            bfs(i,j)
+            bfs(i, j)
+            
+house_num.sort()
+print(len(house_num))
+for i in range(len(house_num)):
+    print(house_num[i])
