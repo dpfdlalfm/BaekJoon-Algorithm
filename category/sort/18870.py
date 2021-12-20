9be716109f5bb87112fc28282fae54b32619ddb8
dpfdlalfm <dpfdlalfm@naver.com>
2021-12-20 16:37:39 +0900

    Create 18870.py

---
+++ category/sort/18870.py
@@ -0,0 +1,28 @@
+# 백준
+# 카테고리별 문제 - 정렬
+# 18870번 좌표 압축
+import sys
+
+
+def compress(arr):
+    lst = sorted(arr)
+    lst[0][1] = 0
+    for i in range(1,len(lst)):
+        if lst[i-1][0] < lst[i][0]:
+            lst[i][1] = lst[i-1][1] + 1
+        else:
+            lst[i][1] = lst[i-1][1]
+
+    for j in x_lst:
+        print(j[1], end=' ')
+
+
+def num_restrict(x,a,b):
+    if x<a or x>b:
+        sys.exit()
+
+
+n = int(input())
+num_restrict(n,1,1000000)
+x_lst = [[x,x] for x in list(map(int,sys.stdin.readline().split()))]
+compress(x_lst)
